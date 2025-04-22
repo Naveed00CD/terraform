9ccec52659dd11b02837d03ca8d23852298d5174
Naveed00CD <na982498@gmail.com>
2025-04-22 21:00:10 +0530

    day 5 files

--- day5-remote-file-dy/backend.tf
+++ day5-remote-file-dy/backend.tf
@@ -4,7 +4,7 @@ backend "s3" {
     bucket         = "serxdcfvgbhjncgfh"
     region         =  "ap-south-1"
     key            = "terraform.tfstate" # Path within the bucket where the state will be read/written.
-    dynamodb_table = "terraform-state-lock-dynamo" # DynamoDB table used for state locking, note: first run Day4-s3bucket-DynamoDB-create-for-backend-statefile-remote-locking
+    dynamodb_table = "navvedddk1k1k1" # DynamoDB table used for state locking, note: first run Day4-s3bucket-DynamoDB-create-for-backend-statefile-remote-locking
     encrypt        = true  # Ensures the state is encrypted at rest in S3.
 }
 }
--- day5-remote-file-dy/main.tf
+++ day5-remote-file-dy/main.tf
@@ -1,10 +1,31 @@
-resource "aws_instance" "day-4" {
-    ami =var.ami
-    instance_type = var.instance_type
+# resource "aws_instance" "day-4" {
+#     ami =var.ami
+#     instance_type = var.instance_type
+  
+# }
+# resource "aws_vpc" "name" {
+#   cidr_block = "10.0.0.0/16"
+ # depends_on = [ aws_instance.day-4 ]
+  
+#  }
+#creating s3 bucket and dynamo DB for state backend storgae and applying the lock mechanisam for statefile
+provider "aws" {
   
 }
-resource "aws_vpc" "name" {
-  cidr_block = "10.0.0.0/16"
-  depends_on = [ aws_instance.dev ]
+
+resource "aws_s3_bucket" "example" {
+  bucket = "serxdcfvgbhjncgfh"
   
+}
+
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name = "navvedddk1k1k1"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+ 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
