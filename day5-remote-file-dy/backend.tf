7ff97b1f1ac5b8a6f1048047c96a6cee8ed06f4b
Naveed00CD <na982498@gmail.com>
2025-04-22 22:33:36 +0530

    complted satate file locking

--- day5-remote-file-dy/backend.tf
+++ day5-remote-file-dy/backend.tf
@@ -1,7 +1,7 @@
 
  terraform {
 backend "s3" {
-    bucket         = "serxdcfvgbhjncgfh"
+    bucket         = "serxdcfvgbhjncgfhlk"
     region         =  "ap-south-1"
     key            = "terraform.tfstate" # Path within the bucket where the state will be read/written.
     dynamodb_table = "navvedddk1k1k1" # DynamoDB table used for state locking, note: first run Day4-s3bucket-DynamoDB-create-for-backend-statefile-remote-locking
